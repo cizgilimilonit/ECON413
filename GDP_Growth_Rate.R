eecb15b8293aea5b2e126d83aff8f7cdc4a9546e
cizgilimilonit <37915518+cizgilimilonit@users.noreply.github.com>
2021-11-26 19:05:04 +0300

    Add files via upload

---
+++ GDP_Growth_Rate.R
@@ -0,0 +1,24 @@
+# Importing the libraries we will use
+install.packages("WDI")
+library(WDI)
+library(data.table)
+
+# Search for the indicator and its code
+WDIsearch('growth')
+
+# Creating our dataframe with the help of WDI library
+dat <- WDI(indicator='NY.GDP.PCAP.KD.ZG', country=c('TR',"CN",'DE','KR','US'), start=2015, end=2020)
+
+# Creating out data table based on dataframe named 'dat'
+das <- data.table("Country" = c("China", "Germany", "South Korea", "Turkey", "United States"),
+                  "2015 GDP" = dat$NY.GDP.PCAP.KD.ZG[dat$year == "2015"],
+                  "2016 GDP" = dat$NY.GDP.PCAP.KD.ZG[dat$year == "2016"],
+                  "2017 GDP" = dat$NY.GDP.PCAP.KD.ZG[dat$year == "2017"],
+                  "2018 GDP" = dat$NY.GDP.PCAP.KD.ZG[dat$year == "2018"],
+                  "2019 GDP" = dat$NY.GDP.PCAP.KD.ZG[dat$year == "2019"],
+                  "2020 GDP" = dat$NY.GDP.PCAP.KD.ZG[dat$year == "2020"])
+# Printing the data table
+print(das)
+
+
+
