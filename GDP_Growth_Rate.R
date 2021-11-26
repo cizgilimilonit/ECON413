a63dacd0bed1040c1613bda5e1272179eefb3fc4
cizgilimilonit <37915518+cizgilimilonit@users.noreply.github.com>
2021-11-26 19:07:09 +0300

    Update GDP_Growth_Rate.R

--- GDP_Growth_Rate.R
+++ GDP_Growth_Rate.R
@@ -10,6 +10,7 @@ WDIsearch('growth')
 dat <- WDI(indicator='NY.GDP.PCAP.KD.ZG', country=c('TR',"CN",'DE','KR','US'), start=2015, end=2020)
 
 # Creating out data table based on dataframe named 'dat'
+# Since WDI brings data in alphabetical order for the countries if we set "Country" column in alphabetical order we will not have an unintentional wrong data 
 das <- data.table("Country" = c("China", "Germany", "South Korea", "Turkey", "United States"),
                   "2015 GDP" = dat$NY.GDP.PCAP.KD.ZG[dat$year == "2015"],
                   "2016 GDP" = dat$NY.GDP.PCAP.KD.ZG[dat$year == "2016"],
