36eb2b9f99905fab7faaa3321b04ffecfefc5403
cizgilimilonit <37915518+cizgilimilonit@users.noreply.github.com>
2021-11-26 19:10:47 +0300

    Update GDP_Growth_Rate.R

--- GDP_Growth_Rate.R
+++ GDP_Growth_Rate.R
@@ -12,12 +12,12 @@ dat <- WDI(indicator='NY.GDP.PCAP.KD.ZG', country=c('TR',"CN",'DE','KR','US'), s
 # Creating out data table based on dataframe named 'dat'
 # Since WDI brings data in alphabetical order for the countries if we set "Country" column in alphabetical order we will not have an unintentional wrong data 
 das <- data.table("Country" = c("China", "Germany", "South Korea", "Turkey", "United States"),
-                  "2015 GDP" = dat$NY.GDP.PCAP.KD.ZG[dat$year == "2015"],
-                  "2016 GDP" = dat$NY.GDP.PCAP.KD.ZG[dat$year == "2016"],
-                  "2017 GDP" = dat$NY.GDP.PCAP.KD.ZG[dat$year == "2017"],
-                  "2018 GDP" = dat$NY.GDP.PCAP.KD.ZG[dat$year == "2018"],
-                  "2019 GDP" = dat$NY.GDP.PCAP.KD.ZG[dat$year == "2019"],
-                  "2020 GDP" = dat$NY.GDP.PCAP.KD.ZG[dat$year == "2020"])
+                  "2015 GDP Growth" = dat$NY.GDP.PCAP.KD.ZG[dat$year == "2015"],
+                  "2016 GDP Growth" = dat$NY.GDP.PCAP.KD.ZG[dat$year == "2016"],
+                  "2017 GDP Growth" = dat$NY.GDP.PCAP.KD.ZG[dat$year == "2017"],
+                  "2018 GDP Growth" = dat$NY.GDP.PCAP.KD.ZG[dat$year == "2018"],
+                  "2019 GDP Growth" = dat$NY.GDP.PCAP.KD.ZG[dat$year == "2019"],
+                  "2020 GDP Growth" = dat$NY.GDP.PCAP.KD.ZG[dat$year == "2020"])
 # Printing the data table
 print(das)
 
